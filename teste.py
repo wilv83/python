3b7918f60efedb01b8f68fa9a057d638b2c6bfdd
wilv83 <wilv83@gmail.com>
2018-11-14 12:40:38 -0200

    Update teste.py

---
+++ teste.py
@@ -0,0 +1,29 @@
+id = 18
+if id < 12:
+            print('a')
+elif id < 18:
+            print('b')
+elif id < 60:
+            print('ç')
+else:
+            print('d')
+	
+print(' ')	
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+ 
+print(' ')
+fruits = ["a", "b", "c"]
+for x in fruits:
+  print(x)
+  
+print(' ')
+list_of_lists = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
+for list in list_of_lists:
+    for xa in list:
+        print (xa)
+		
+
+
